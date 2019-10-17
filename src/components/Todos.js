1c4b2c863267fec416cffa9fce02db40bdf3e34b
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-17 22:48:59 +0100

    added list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 import PropTypes from "prop-types";
+import { List } from "semantic-ui-react";
 
 class Todos extends Component {
   render() {
-    return this.props.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
+    return (
+      <List as="ul">
+        return this.props.todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+        );
+      </List>
+    );
   }
 }
 
